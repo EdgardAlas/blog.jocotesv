b9d6ac58fe7dd8a2ca2ebfb0e90b685c3a2cf675
Edgard Alas <edgardalasu@gmail.com>
2024-12-26 19:54:41 -0600

    feat: add Card component and implement Admin Home Page layout

--- src/app/admin/(home)/page.tsx
+++ src/app/admin/(home)/page.tsx
@@ -1,5 +1,27 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+
 const AdminHomePage = () => {
-	return <></>;
+	return (
+		<>
+			<Card>
+				<CardHeader>
+					<CardTitle>Welcome to Admin Panel</CardTitle>
+					<CardDescription>
+						Here you can manage your application
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p>You can manage your application from here.</p>
+				</CardContent>
+			</Card>
+		</>
+	);
 };
 
 export default AdminHomePage;
