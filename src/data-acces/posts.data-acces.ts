ae0a37f4ee3c447b5a0c9e943b322b7773976b3e
Edgard Alas <edgardalasu@gmail.com>
2025-01-16 16:56:29 -0600

    fix: update deletePost logic to also remove posts with matching parentId

--- src/data-acces/posts.data-acces.ts
+++ src/data-acces/posts.data-acces.ts
@@ -153,7 +153,7 @@ export const deletePost = async (
 	id: string,
 	tx: Transaction | typeof db = db
 ): Promise<void> => {
-	await tx.delete(posts).where(eq(posts.id, id));
+	await tx.delete(posts).where(or(eq(posts.id, id), eq(posts.parentId, id)));
 };
 
 export const countPosts = async (
